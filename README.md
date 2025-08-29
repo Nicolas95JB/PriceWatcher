bad12f490c561757c8c524bb7c0753ef567c3525
Nicolas95JB <njbasalo@gmail.com>
2025-08-29 02:17:14 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# PriceWatcher
+# Documento funcional
+## caso de uso
+1. Home: Automaticamente tomar seccion "BAJARON DE PRECIO" o "PRODUCTOS DESTACADOS". (definir N paginas)
+2. Permitir la busqueda
+    - contemplar no resultados.
+3. (CORE) Permitir definir una busqueda para un par de horas o un dia max.
+    - a. Se debe poder definir un presupuesto de alerta.
+    - b. Trackear item con menor precio y notificar de subida o bajada.
+
+defino alerta -> busqueda:"monitor lg 27", presupuesto:"400k"
+sumar la definicion de lapso? (1d, 12hs, 6hs)
+
+# Ideas
+- definir palabras clave para manejar "categorias"
---
+++ main.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulSoup
+import sys
+import requests
+import asyncio
+
+# seccion clases
+class product:
+    def __init__(self, title, price, shop):
+        self.title = title
+        self.price = price
+        self.shop = shop
+    def info(self):
+        return f"Product: {self.title} \nPrice: {self.price}\nShop: {self.shop}"
+# fin seccion clases
+
+# seccion funciones
+async def app():
+    trys = 0
+    try:
+        while trys < 5:
+            trys += 1
+            res = requests.get("https://www.hardgamers.com.ar/deals?page=1")
+            if res.status_code == 200:
+                return extract_info(BeautifulSoup(res.text, "lxml"))
+            await asyncio.sleep(300)
+    except requests.RequestException as e:
+        print(f"Error al realizar la solicitud: {e}")
+    finally:
+        print(f"Solicitud finalizada. luego de {trys} intengos.")
+
+def extract_info(soup):
+    articles = soup.find_all("article")
+    for article in articles:
+        title = article.find(class_="product-title").text.strip()
+        shop = article.find(class_="subtitle").text.strip()
+        price_obj = article.find_all(class_="product-price")
+        price_text = ' '.join([p.text.strip() for p in price_obj])
+        print(f"Titulo: {title}\nPrecio: {price_text}\nTienda: {shop}\n")
+
+# fin seccion funciones
+
+if __name__ == "__main__":
+    asyncio.run(app())
